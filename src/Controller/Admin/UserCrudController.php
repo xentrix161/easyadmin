f978e40fdd3df8f007c13e7f456ff648c199f949
xentrix <filip.kosmel@maurit.sk>
2023-10-09 10:06:21 +0200

    avatarfield

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AvatarField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
@@ -23,10 +24,16 @@ class UserCrudController extends AbstractCrudController
     {
         yield IdField::new('id')
             ->onlyOnIndex();
+        yield AvatarField::new('avatar')
+            ->formatValue(static function ($value, User $user) {
+                return $user->getAvatarUrl();
+            })
+            ->hideOnForm();
         yield ImageField::new('avatar')
             ->setBasePath('uploads/avatars')
             ->setUploadDir('public/uploads/avatars')
-            ->setUploadedFileNamePattern('[slug]-[timestamp].[extension]');
+            ->setUploadedFileNamePattern('[slug]-[timestamp].[extension]')
+            ->onlyOnForms();
         yield EmailField::new('email');
         yield TextField::new('fullName')
             ->hideOnForm();
